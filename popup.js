984e5111325435f8382175ce76b4a79fbb5fbcef
markmliu <markmliu@umich.edu>
2013-07-30 00:40:54 -0700

    translations work now

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
-
+//	listen for changes to language selection	
+	document.addEventListener('DOMContentLoaded', function(){
+		$("#langselect").on("change",redefine);
+	});
 
 window.onload = function() {     
     var word = localStorage.text;
@@ -6,10 +9,7 @@ window.onload = function() {
     var cust = document.getElementById("custom");
 	var defcontainer = document.getElementById("fromdict");
     cust.innerHTML="Word: "+ word;
-//	listen for changes to language selection	
-	document.addEventListener('DOMContentLoaded', function(){
-		document.getElementById('langselect').addEventListener('onchange',redefine(this));
-	});
+
 	
 	var xhr = new XMLHttpRequest();
 	var url = "http://glosbe.com/gapi/translate?from=eng&dest=eng&format=json&phrase="+word+"&pretty=true";
@@ -27,9 +27,14 @@ window.onload = function() {
 	xhr.send();
 	
 };
-function redefine(sel) {
+function redefine() {
+	var word = localStorage.text;
+	word = word.toLowerCase();
+	var defcontainer = document.getElementById("fromdict");
+	var sel = $("#langselect").val();
+	defcontainer.innerHTML = "Switched to "+sel;
 	var xhr = new XMLHttpRequest();
-	var url = "http://glosbe.com/gapi/translate?from=eng&dest=cmn&format=json&phrase="+word+"&pretty=true";
+	var url = "http://glosbe.com/gapi/translate?from=eng&dest="+sel+"&format=json&phrase="+word+"&pretty=true";
 //    var url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/hypocrite?key=94cf6c42-8f95-4eb1-be29-179e664a470c";
 
 	xhr.open("GET", url, true);
@@ -37,7 +42,7 @@ function redefine(sel) {
 		if (xhr.readyState == 4) {
 			// JSON.parse does not evaluate the attacker's scripts.
 			var resp = JSON.parse(xhr.responseText);
-			var def = "Definition: "+resp["tuc"][0]["meanings"][0]["text"];
+			var def = "Translation: "+resp["tuc"][0]["phrase"]["text"];
 			defcontainer.innerHTML = def;
 		}
 	}
